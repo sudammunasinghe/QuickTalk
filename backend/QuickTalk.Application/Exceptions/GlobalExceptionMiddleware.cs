791ee13341ea19e724fca255356a6cd338631d77
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-05-19 12:35:35 +0530

    global exception handling

---
+++ backend/QuickTalk.Application/Exceptions/GlobalExceptionMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using QuickTalk.Application.DTOs.ApiResponse;
+using QuickTalk.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QuickTalk.Application.Exceptions
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                await HandleExceptionAsync(ex, context);
+            }
+        }
+
+        private Task HandleExceptionAsync(Exception exception, HttpContext context)
+        {
+            string message = exception.Message;
+            HttpStatusCode status;
+
+            switch (exception)
+            {
+                case NotFoundException:
+                    status = HttpStatusCode.NotFound;
+                    break;
+                
+                case UnauthorizedAccessException:
+                    status = HttpStatusCode.Unauthorized;
+                    break;
+
+                case BadRequestException:
+                    status = HttpStatusCode.BadRequest;
+                    break;
+
+                case ConflictException:
+                    status = HttpStatusCode.Conflict;
+                    break;
+
+                default:
+                    status = HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var response = new ApiResponse<string>
+            {
+                IsSuccess = false,
+                Message = message
+            };
+
+            var payload = JsonSerializer.Serialize(response);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+            return context.Response.WriteAsync(payload);
+        }
+    }
+}
---
+++ backend/QuickTalk.Infrastructure/Identity/CurrentUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using QuickTalk.Application.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickTalk.Infrastructure.Identity
+{
+    public class CurrentUser : IcurrentUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        
+        public int? UserId
+        {
+            get
+            {
+                var userIdClaim = _httpContextAccessor.HttpContext?
+                    .User.FindFirst(ClaimTypes.NameIdentifier);
+
+                if (userIdClaim == null)
+                    throw new UnauthorizedAccessException("Unauthorized .");
+
+                return int.Parse(userIdClaim.Value);
+            }
+        }
+    }
+}
