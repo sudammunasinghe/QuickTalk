de3b9b5b790e80052e9facd0459069105c8a8ba3
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-05-23 20:17:39 +0530

    project structure

---
+++ frontend/quick-talk/src/app/features/chat/chat-header/chat-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-header',
+  imports: [],
+  templateUrl: './chat-header.html',
+  styleUrl: './chat-header.scss',
+})
+export class ChatHeader {}
---
+++ frontend/quick-talk/src/app/features/chat/message-input/message-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-input',
+  imports: [],
+  templateUrl: './message-input.html',
+  styleUrl: './message-input.scss',
+})
+export class MessageInput {}
