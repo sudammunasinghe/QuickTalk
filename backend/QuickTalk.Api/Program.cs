f2cee3dccc2c1e40f1bc16daa06dc74de218ff1c
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-05-24 17:15:06 +0530

    add CORS

--- backend/QuickTalk.Api/Program.cs
+++ backend/QuickTalk.Api/Program.cs
@@ -35,6 +35,19 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
 
+// ================================== CORS Configuration ==================================================
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 
 //===================================== JWT Authetication ==================================================
 
@@ -113,6 +126,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularApp");
 
 app.UseAuthentication();
 
