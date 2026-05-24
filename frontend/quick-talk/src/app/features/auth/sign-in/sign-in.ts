f6c1ad885d4995456b0611347ddc89f9a489c744
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-05-24 14:53:03 +0530

    sign in form ui

--- frontend/quick-talk/src/app/features/auth/sign-in/sign-in.ts
+++ frontend/quick-talk/src/app/features/auth/sign-in/sign-in.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password'
+import { CardModule } from 'primeng/card';
+import { 
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup
+} from '@angular/forms'
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink],
+  imports: [
+    RouterLink, 
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    CardModule
+  ],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.scss',
 })
-export class SignIn {}
+export class SignIn {
+signInForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+
+  }
+
+  onSubmit() {
+
+    if(this.signInForm.invalid){
+      return;
+    }
+
+    console.log(this.signInForm.value);
+  }
+}
