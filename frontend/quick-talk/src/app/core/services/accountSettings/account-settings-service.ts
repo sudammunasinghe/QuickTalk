4c4ba01d88f4239ddd7e21d6b1a33c45eb88828f
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-06-30 18:40:12 +0530

    change password data binding and service

---
+++ frontend/quick-talk/src/app/core/services/accountSettings/account-settings-service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { ChangePasswordRequest } from '../../models/account/change-password-request';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../../models/apiResponse/api-response';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AccountSettingsService {
+    private http = inject(HttpClient);
+    private apiUrl = `${environment.apiUrl}/accountSettings`;
+
+    ChangePasswordAsync(request: ChangePasswordRequest): Observable<ApiResponse<string>> {
+        return this.http.post<ApiResponse<string>>(
+            `${this.apiUrl}/change-password`,
+            request
+        );
+    }
+}
--- frontend/quick-talk/src/app/features/chat/components/sidebar-header/account-settings-dialog/password-settings/password-settings.ts
+++ frontend/quick-talk/src/app/features/chat/components/sidebar-header/account-settings-dialog/password-settings/password-settings.ts
@@ -1,18 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { PasswordModule } from 'primeng/password';
+import { AccountSettingsService } from '../../../../../../core/services/accountSettings/account-settings-service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ChangePasswordRequest } from '../../../../../../core/models/account/change-password-request';
 
 @Component({
-  selector: 'app-password-settings',
-  imports: [
-    ButtonModule,
-    InputTextModule,
-    CardModule,
-    PasswordModule
-  ],
-  templateUrl: './password-settings.html',
-  styleUrl: './password-settings.scss',
+    selector: 'app-password-settings',
+    imports: [
+        ButtonModule,
+        InputTextModule,
+        CardModule,
+        PasswordModule,
+        ReactiveFormsModule
+    ],
+    templateUrl: './password-settings.html',
+    styleUrl: './password-settings.scss',
 })
-export class PasswordSettings {}
+export class PasswordSettings {
+    @Output() close = new EventEmitter<void>();
+    changePasswordForm!: FormGroup;
+    constructor(
+        private settingsService: AccountSettingsService,
+        private fb: FormBuilder,
+        private messageService: MessageService
+    ) {
+        this.changePasswordForm = this.fb.group({
+            currentPassword: ['', [Validators.required]],
+            newPassword: ['', [Validators.required]],
+            confirmNewPassword: ['', [Validators.required]]
+        });
+    }
+
+    changePassword(): void {
+        if (this.changePasswordForm.invalid)
+            return;
+
+        const changePasswordRequest: ChangePasswordRequest = {
+            currentPassword: this.changePasswordForm.value.currentPassword,
+            newPassword: this.changePasswordForm.value.newPassword,
+            confirmedNewPassword: this.changePasswordForm.value.confirmNewPassword
+        };
+
+        this.settingsService.ChangePasswordAsync(changePasswordRequest)
+            .subscribe({
+                next: (response) => {
+                    this.changePasswordForm.reset();
+                    this.close.emit();
+                    if (response.isSuccess) {
+                        this.messageService.add({
+                            severity: 'success',
+                            summary: 'success',
+                            detail: response.message
+                        });
+                    }
+                },
+                error: (response) => {
+                    this.changePasswordForm.reset();
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'error',
+                        detail: response.error.Message
+                    });
+                }
+            })
+    }
+}
+
