3a98a8e90378538643a22d61dee9ed1f2004f3d3
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-07-05 12:10:20 +0530

    manage privacy settings - FE changes

--- frontend/quick-talk/src/app/core/models/chat/chat-item-response.ts
+++ frontend/quick-talk/src/app/core/models/chat/chat-item-response.ts
@@ -4,6 +4,10 @@ export interface ChatItemResponse {
     lastName: string,
     lastMessage: string,
     lastMessageDisplayTime: string,
+    showProfilePicture: boolean,
+    showOnlineStatus: boolean,
+    showLastSeen: boolean,
+    showBio: boolean,
     unreadCount: number,
     isOnline: boolean
 }
