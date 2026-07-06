b60b3eddee23dafebabe3633c6cc7c4c546c6c05
sudammunasinghe <ssmunasinghe1998@gmail.com>
2026-07-06 14:55:40 +0530

    profile-settings ui - FE Changes

--- frontend/quick-talk/src/app/features/chat/components/sidebar-header/account-settings-dialog/profile-settings/profile-settings.ts
+++ frontend/quick-talk/src/app/features/chat/components/sidebar-header/account-settings-dialog/profile-settings/profile-settings.ts
@@ -1,9 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { TextareaModule } from 'primeng/textarea';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
-  selector: 'app-profile-settings',
-  imports: [],
-  templateUrl: './profile-settings.html',
-  styleUrl: './profile-settings.scss',
+    selector: 'app-profile-settings',
+    imports: [
+        CommonModule,
+        ButtonModule,
+        TextareaModule,
+        InputTextModule,
+        PasswordModule,
+        ReactiveFormsModule,
+        FileUploadModule
+    ],
+    templateUrl: './profile-settings.html',
+    styleUrl: './profile-settings.scss',
 })
-export class ProfileSettings {}
+export class ProfileSettings { 
+    defaultImage = 'assets/images/profile.png'
+}
